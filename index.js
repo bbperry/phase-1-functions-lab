beab8f3c6d2e3e620234d1e780c00f4a4748a258
Ben <benbperry1@gmail.com>
2021-11-03 09:12:40 -0700

    commit tree 0d0937bb9f3d6b5616b203d58da18ae879d99a01

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(location) {
+    return Math.abs(42-location)  
+}
+
+function distanceFromHqInFeet(location) {
+    return (Math.abs(42-location))* 264 
+}
+
+function distanceTravelledInFeet(start, end) {
+    return (Math.abs(start-end))* 264
+}
+
+function calculatesFarePrice(start, destination) {
+    let distance = (Math.abs(start-destination))* 264;
+
+        if (distance <= 400) {
+            return distance * 0
+         } else if (distance > 400 && distance < 2000) {
+             return (distance-400)*.02
+        }  else if (distance > 2000 && distance < 2500) {
+             return 25 
+        } else if (distance > 2500) {
+            return 'cannot travel that far'
+        }
+}
